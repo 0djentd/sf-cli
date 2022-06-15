8a53cfab9703db23ce34dd567bd6fb2a571cdb49
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-15 23:39:37 +0300

    modified:   .gitignore modified:   src/__main__.py modified:   src/core.py modified:   src/flashcards.py

--- src/core.py
+++ src/core.py
@@ -1,6 +1,20 @@
 import logging
-import sqlalchemy
+
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import declarative_base
 
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+
+Base = declarative_base()
+
+
+def create_db(db_file: str = "/db.sqlite3") -> Engine:
+    db_file = "sqlite://" + db_file
+    logger.info(f"Creating db at {db_file}")
+    engine = create_engine(db_file, echo=True)
+    Base.metadata.create_all(engine)
+    return engine
--- src/flashcards.py
+++ src/flashcards.py
@@ -2,6 +2,16 @@ import logging
 import datetime
 import sqlalchemy
 
+from . core import Base
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+
+class Flashcard(Base):
+    __tablename__ = "flashcard"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    front = sqlalchemy.Column(sqlalchemy.Text)
+    back = sqlalchemy.Column(sqlalchemy.Text)
