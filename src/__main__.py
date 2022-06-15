98eec5f3b834a29406c7396de2a53b2d1a74d021
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-16 00:23:01 +0300

    modified:   src/__main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,20 @@
+import logging
+
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+
 from .core import create_db, Base
+from .flashcards import Flashcard
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
+
 
 engine = create_db()
+
+with Session(engine) as session:
+    fcs = select(Flashcard)
+    for x in session.scalars(fcs):
+        logger.debug(x)
